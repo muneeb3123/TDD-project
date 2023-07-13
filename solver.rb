931150fcede13222d7add7926a76f4e7121fbe97
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-07-14 00:26:41 +0500

    write code to pass failed test

--- solver.rb
+++ solver.rb
@@ -13,4 +13,10 @@ class Solver
   def reverse(str)
     str.reverse
   end
+
+  def fizzbuzz(num)
+    return 'fizzbuzz' if (num % 3).zero? && (num % 5).zero?
+    return 'fizz' if (num % 3).zero?
+    return 'buzz' if (num % 5).zero?
+  end
 end
