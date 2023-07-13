6798c7dad9be739d6a7547f61a4a84104182987b
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-07-13 22:05:10 +0500

    write test for factorial

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,3 +1,15 @@
-describe Solver do
-    
+require_relative '../solver'
+
+RSpec.describe Solver do
+  subject { described_class.new }
+
+    it 'should take an integer argument and return its factorial' do
+      result = subject.factorial(5)
+      expect(result).to eql(120)
+    end
+
+    it 'it should have special case that the factorial of 0 is 1.' do
+        result = subject.factorial(0)
+        expect(result).to eql(1)
+    end
 end
