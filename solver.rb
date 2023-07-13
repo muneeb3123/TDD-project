70f750e155d75e039db71d4fef041fd1614c01f4
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-07-13 22:18:04 +0500

    modify

--- solver.rb
+++ solver.rb
@@ -1,11 +1,12 @@
 class Solver
-    def factorial(num)
-        raise 'Factorial is not defined for negative numbers' if num < 0
-        return 1 if num == 0
-        result = 1
-        (2..num).each do |i|
-            result *= i
-        end
-        result
+  def factorial(num)
+    raise 'Factorial is not defined for negative numbers' if num.negative?
+    return 1 if num.zero?
+
+    result = 1
+    (2..num).each do |i|
+      result *= i
     end
+    result
+  end
 end
