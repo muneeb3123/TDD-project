33b2f6cda1fc21096b1a0657f24783c26bde1949
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-07-13 22:16:06 +0500

    write final test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -12,4 +12,8 @@ RSpec.describe Solver do
         result = subject.factorial(0)
         expect(result).to eql(1)
     end
+
+    it 'it should raise exception if num is negative' do
+        expect {subject.factorial(-3)}.to raise_error('Factorial is not defined for negative numbers')
+    end
 end
